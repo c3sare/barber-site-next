6a001d8d8d653a516f5b4fdcdbdc8d2a5cdf445e
C3sare <marcinm222@gmail.com>
2024-03-04 15:51:58 +0100

    added nodemailer

---
+++ lib/mail.ts
@@ -0,0 +1,13 @@
+// import nodemailer from "nodemailer";
+
+// export const mail = nodemailer.createTransport({
+//   host: process.env.NODEMAILER_EMAIL_HOST as string,
+//   port: Number(process.env.NODEMAILER_EMAIL_PORT),
+//   secure: Boolean(process.env.NODEMAILER_EMAIL_SECURE),
+//   auth: {
+//     type: "OAuth2",
+//     user: process.env.NODEMAILER_EMAIL_USER,
+//     clientId: process.env.NODEMAILER_EMAIL_CLIENT_ID,
+//     privateKey: process.env.NODEMAILER_EMAIL_PRIVATE_KEY,
+//   },
+// });

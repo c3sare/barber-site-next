d604982f43285da53277ec4edb8ee04c4c8e03d9
Marcin Marciniuk <marcinm222@gmail.com>
2024-05-03 21:42:05 +0200

    env changes

--- lib/drizzle.ts
+++ lib/drizzle.ts
@@ -3,6 +3,6 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "@/drizzle/schema";
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(process.env.POSTGRES_URL!);
 const db = drizzle(sql, { schema });
 export default db;

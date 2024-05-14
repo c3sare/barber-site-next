77e6dad5b602fe08db2fcfd24f21df14b43f6fa6
Marcin Marciniuk <marcinm222@gmail.com>
2024-05-14 17:58:17 +0200

    middlware

--- middleware.ts
+++ middleware.ts
@@ -1,52 +1,52 @@
-import {
-  DEFAULT_LOGIN_REDIRECT,
-  apiAuthPrefix,
-  authRoutes,
-  protectedRoutes,
-} from "@/routes";
-import includesStartsWith from "./utils/includesStartsWith";
+// import {
+//   DEFAULT_LOGIN_REDIRECT,
+//   apiAuthPrefix,
+//   authRoutes,
+//   protectedRoutes,
+// } from "@/routes";
+// import includesStartsWith from "./utils/includesStartsWith";
 import authConfig from "./auth";
 import NextAuth from "next-auth";
 
-const { auth } = NextAuth(authConfig);
+export const { auth: middleware } = NextAuth(authConfig);
 
-export default auth((req) => {
-  const { nextUrl } = req;
-  const isLoggedIn = !!req.auth;
+// export default auth((req) => {
+//   const { nextUrl } = req;
+//   const isLoggedIn = !!req.auth;
 
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isProtectedRoute = includesStartsWith(
-    protectedRoutes,
-    nextUrl.pathname
-  );
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+//   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+//   const isProtectedRoute = includesStartsWith(
+//     protectedRoutes,
+//     nextUrl.pathname
+//   );
+//   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  if (isApiAuthRoute) {
-    return;
-  }
+//   if (isApiAuthRoute) {
+//     return;
+//   }
 
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-    }
-    return;
-  }
+//   if (isAuthRoute) {
+//     if (isLoggedIn) {
+//       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+//     }
+//     return;
+//   }
 
-  if (!isLoggedIn && isProtectedRoute) {
-    let callbackUrl = nextUrl.pathname;
-    if (nextUrl.search) {
-      callbackUrl += nextUrl.search;
-    }
+//   if (!isLoggedIn && isProtectedRoute) {
+//     let callbackUrl = nextUrl.pathname;
+//     if (nextUrl.search) {
+//       callbackUrl += nextUrl.search;
+//     }
 
-    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+//     const encodedCallbackUrl = encodeURIComponent(callbackUrl);
 
-    return Response.redirect(
-      new URL(`/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
-    );
-  }
+//     return Response.redirect(
+//       new URL(`/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+//     );
+//   }
 
-  return;
-});
+//   return;
+// });
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],

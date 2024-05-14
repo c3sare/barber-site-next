9cfd2ed52a053da471b4bf456faed4703f05c728
C3sare <marcinm222@gmail.com>
2024-05-14 14:20:53 +0200

    railway build

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "lh3.googleusercontent.com",
-			},
-			{
-				protocol: "https",
-				hostname: "res.cloudinary.com",
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  output: process.env.RAILWAY === "true" ? "standalone" : undefined,
 };
 
 export default nextConfig;

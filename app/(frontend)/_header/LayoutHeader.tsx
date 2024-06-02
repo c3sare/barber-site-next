9e13f4c520c8e84b849f4bb719acf564de34b9e6
Marcin Marciniuk <marcinm222@gmail.com>
2024-06-02 17:46:13 +0200

    push

--- app/(frontend)/_header/LayoutHeader.tsx
+++ app/(frontend)/_header/LayoutHeader.tsx
@@ -7,88 +7,18 @@ import Header from "./_components/Header";
 import Navigation from "./_components/Navigation";
 import NavLinkHeaderChild from "./_components/NavLinkHeaderChild";
 import Account from "./_components/Account";
+import db from "@/lib/drizzle";
 
-const routing = [
-  {
-    href: "/",
-    title: "Home",
-  },
-  {
-    href: "/barbers",
-    title: "Barbers",
-  },
-  {
-    href: "/services",
-    title: "Services",
-  },
-  {
-    href: "/shop",
-    title: "Shop",
-    children: [
-      {
-        href: "/shop-catalog",
-        title: "Shop Catalog",
-      },
-      {
-        href: "/shop-sidebar",
-        title: "Shop Sidebar",
-      },
-      {
-        href: "/shop-single",
-        title: "Shop Single",
-      },
-      {
-        href: "/shop-single-alt",
-        title: "Shop Single Alt",
-      },
-    ],
-  },
-  {
-    href: "/gallery",
-    title: "Gallery",
-    children: [
-      {
-        href: "/masonry-style",
-        title: "Masonry Style",
-      },
-      {
-        href: "/fullwidth-grid",
-        title: "Fullwidth grid",
-      },
-      {
-        href: "/2-columns",
-        title: "2 Columns",
-      },
-      {
-        href: "/3-columns",
-        title: "3 Columns",
-      },
-      {
-        href: "/3-columns",
-        title: "3 Columns",
-      },
-      {
-        href: "/single-gallery",
-        title: "Single Gallery",
-      },
-    ],
-  },
-  {
-    href: "/blog",
-    title: "Blog",
-  },
-  {
-    href: "/contact",
-    title: "Contact",
-  },
-  {
-    href: "/book-now",
-    title: "Book now",
-    label: "Hot",
-  },
-];
+const LayoutHeader = async () => {
+  const menu = await db.query.menu.findFirst({
+    where: (menu, { eq }) => eq(menu.usedBy, "HEADER"),
+    with: {
+      items: true,
+    },
+  });
 
-const LayoutHeader = () => {
+  const menuItems = menu?.items ?? [];
+
   return (
     <Header>
       <nav className="max-w-7xl mx-auto flex justify-between items-center px-4 flex-wrap md:flex-nowrap">
@@ -102,14 +32,13 @@ const LayoutHeader = () => {
           />
         </Link>
         <Navigation account={<Account />}>
-          {routing.map((route, i) => (
-            <NavLinkHeader key={i} {...route}>
-              {route.children
-                ? route.children?.map((child, childI) => (
-                    <NavLinkHeaderChild key={childI} {...child} />
-                  ))
-                : null}
-            </NavLinkHeader>
+          {menuItems.map((route, i) => (
+            <NavLinkHeader
+              key={i}
+              title={route.name}
+              href={`/page/${route.url}`}
+              {...route}
+            />
           ))}
         </Navigation>
       </nav>
--- app/(frontend)/_header/_components/NavLinkHeader.tsx
+++ app/(frontend)/_header/_components/NavLinkHeader.tsx
@@ -1,71 +1,20 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { useTransition, animated } from "@react-spring/web";
-import { useHeaderHeight } from "./Header";
-import useMediaQuery from "@/hooks/useMediaQuery";
-import { ChevronDown } from "lucide-react";
-import { cn } from "@/lib/utils";
 
 type NavLinkHeaderProps = {
   title: string;
   href: string;
   label?: string;
-  children?: React.ReactNode;
 };
 
 const NavLinkHeader: React.FC<NavLinkHeaderProps> = ({
   title,
   href,
   label,
-  children,
 }) => {
-  const isPhone = useMediaQuery("(max-width: 768px)");
-  const headerHeight = useHeaderHeight();
-  const [isExpanded, setIsExpanded] = useState<boolean>(false);
-  const transition = useTransition(isExpanded, {
-    from: isPhone
-      ? { maxHeight: "0" }
-      : {
-          opacity: 0,
-        },
-    enter: isPhone
-      ? { maxHeight: "100em" }
-      : {
-          display: "flex",
-          opacity: 1,
-        },
-    leave: isPhone
-      ? {
-          maxHeight: "0",
-        }
-      : {
-          opacity: 0,
-          delay: 100,
-        },
-    ...(isPhone ? {} : { duration: 100 }),
-  });
-
-  useEffect(() => {
-    if (!isPhone && isExpanded) setIsExpanded(false);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isPhone]);
-
-  const onHoverItem = () => {
-    setIsExpanded(true);
-  };
-
-  const onHoverItemLeave = () => {
-    setIsExpanded(false);
-  };
-
   return (
-    <li
-      className="relative group min-w-[200px] md:min-w-max"
-      onMouseEnter={children && !isPhone ? () => onHoverItem() : undefined}
-      onMouseLeave={children && !isPhone ? () => onHoverItemLeave() : undefined}
-    >
+    <li className="relative group min-w-[200px] md:min-w-max">
       <span className="relative">
         <Link
           prefetch={false}
@@ -79,31 +28,7 @@ const NavLinkHeader: React.FC<NavLinkHeaderProps> = ({
             </span>
           )}
         </Link>
-        {children ? (
-          <button
-            className="md:hidden absolute right-0 top-1/2 -translate-y-1/2"
-            onClick={() => setIsExpanded(!isExpanded)}
-          >
-            <ChevronDown
-              className={cn(
-                "transition-transform duration-300 w-8 h-8",
-                isExpanded && "rotate-180"
-              )}
-            />
-            <span className="sr-only">Expan {title} menu</span>
-          </button>
-        ) : null}
       </span>
-      {transition((style, item) =>
-        item ? (
-          <animated.ul
-            style={{ ...style, top: headerHeight }}
-            className="flex-col md:border md:border-secondary overflow-hidden md:overflow-visible md:fixed md:top-full md:bg-white md:text-black w-full md:w-[150px] normal-case md:shadow-md"
-          >
-            {children}
-          </animated.ul>
-        ) : null
-      )}
     </li>
   );
 };
